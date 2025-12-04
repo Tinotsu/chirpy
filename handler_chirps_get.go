a7fb7574a509919c0daef94173ce7c2fd0fec6df
Tinotsu <tinopiof@gmail.com>
2025-12-04 15:30:19 +0100

    get chirps sort option

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -4,13 +4,18 @@ import (
 	"net/http"
 	"github.com/google/uuid"
 	"github.com/Tinotsu/chirpy/internal/database"
+	"sort"
+	"strings"
+	"log"
 )
 
 type Chirps []Chirp
 
 func (apiCfg * apiConfig) getChirps (w http.ResponseWriter, r *http.Request) {
-	s := r.URL.Query().Get("author_id")
-	sID,_ := uuid.Parse(s)
+	authorOpt := r.URL.Query().Get("author_id")
+	authorOptID,_ := uuid.Parse(authorOpt)
+
+	sortOPT := r.URL.Query().Get("sort")
 
 	chirpsData, err := apiCfg.db.GetChirps(r.Context())
 	if err != nil {
@@ -26,11 +31,20 @@ func (apiCfg * apiConfig) getChirps (w http.ResponseWriter, r *http.Request) {
 			Body: 	chirp.Body,
 			UserID:	chirp.UserID,
 		}
-		if sID == chirp.UserID {
+		if authorOptID == chirp.UserID {
 			chirps = append(chirps, chirpStruct)
-		} else if s == "" {
+		} else if authorOpt == "" {
 			chirps = append(chirps, chirpStruct)
 		}
+	}
+
+	if strings.Contains(sortOPT, "asc") {
+		log.Println("sort is asc")
+		sort.Slice(chirps, func (i, j int) bool {return chirps[j].UpdatedAt.After(chirps[i].UpdatedAt)})
+	}
+	if strings.Contains(sortOPT, "desc") {
+		log.Println("sort is desc")
+		sort.Slice(chirps, func (i, j int) bool {return chirps[j].UpdatedAt.Before(chirps[i].UpdatedAt)})
 	}
 	respondWithJSON(w, http.StatusOK, chirps) 
 }
